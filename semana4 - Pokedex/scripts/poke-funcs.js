72c7a3d7ddf474e3c3b324c0678ad535a420b537
Zaid Alejandro <sadx_1997_97728@live.com.mx>
2022-03-20 17:44:08 -0600

    Pokedex funcionando

--- semana4 - Pokedex/scripts/poke-funcs.js
+++ semana4 - Pokedex/scripts/poke-funcs.js
@@ -28,18 +28,20 @@ const fetchPokemon = () => {
         }
     }).then((data) => {
         if (data) {
-            console.log(data);
             let pokeImg = data.sprites.other.home.front_default;//imagen
             let pokeNom = data.forms[0].name; //Nombre del pokemon
-            let pokeTipo = data.types[0].type.name; //tipo de pokemon
+            let pokeNumero = data.id; //Nombre del pokemon
+            let pokeTipo = data.types; //tipo de pokemon
             let movimientos = data.moves;// movimientos del pokemon
             let estadisticas = data.stats; // estadisticas del pokemon
+            let weight = data.weight;
+            let height = data.height;
 
             pokeImage(pokeImg);
-            pokeTitle(pokeNom, poketipo);
-            pokeType(poketipo)
-            pokeStatistics(estadisticas)
-            pokeMoves(movimiento)
+            pokeTitle(pokeNom, pokeNumero);
+            pokeType(pokeTipo)
+            pokeStatistics(weight, height, estadisticas)
+            pokeMoves(movimientos)
         }
     });
 }
@@ -49,27 +51,73 @@ const pokeImage = (url) => {
     pokePhoto.src = url;
 }
 
-const pokeTitle = (nombrePokemon, tipoPokemon) => {
-    let nombre;
-    let numero;
+const pokeTitle = (nombrePokemon, tipoPokemon) => { 
+    document.getElementById('pokemonTitleName').textContent = nombrePokemon;
+    document.getElementById('pokeNumber').textContent = "N.°" + tipoPokemon;
 }
 
 const pokeType = (tipoPokemon) => {
-    let tipoUno;
-    let tipoDos;
+    let tiposPokemon = "";
+    tipoPokemon.forEach(element => {
+        tiposPokemon += element.type.name + " ";
+    });
+    document.getElementById('poketipo').textContent = tiposPokemon;
 }
 
-const pokeStatistics = (estadisticasPokemon) => {
-    let peso ; //entre 10 m
-    let altura; // entre 10 kg
-    let ps;
-    let ataque;
-    let defensa
-    let atqEspecial;
-    let DefensaEspecial;
-    let vel;
+const pokeStatistics = (weight, height, estadisticasPokemon) => {
+    document.getElementById('Peso').textContent ="Peso: "+ (weight/10) + "kg";
+    document.getElementById('Altura').textContent ="Altura: "+ (height/10)+ "m";
+    document.getElementById('PS').textContent = "PS: "+ estadisticasPokemon[0].base_stat;
+    document.getElementById('Ataque').textContent ="Ataque: "+ estadisticasPokemon[1].base_stat;
+    document.getElementById('Defensa').textContent ="Defensa: "+ estadisticasPokemon[2].base_stat;
+    document.getElementById('AtaqueEspecial').textContent ="At. Especial: "+ estadisticasPokemon[3].base_stat;
+    document.getElementById('DefensaEspecial').textContent ="Def. especial: "+ estadisticasPokemon[4].base_stat;
+    document.getElementById('Velocidad').textContent ="Velocidad: "+ estadisticasPokemon[5].base_stat;
 }
 
 const pokeMoves = (movimientosPokemon) => {
+    let listaMovimientos = document.getElementById("ListaMovimientos");
+    let movimiento;
+    movimientosPokemon.forEach(movimientos => {
+        movimiento = document.createElement("li");
+        movimiento.appendChild(document.createTextNode(movimientos.move.name));
+        listaMovimientos.appendChild(movimiento); 
+    });
+}
+
+/* Evento para buscar pokemon al pulsar enter*/
+document.getElementById("pokeName")
+.addEventListener("keyup", function(event) {
+console.log("Event  ")
+event.preventDefault();
+if (event.key === 'Enter') {
+    document.getElementById("buscarPokemon").click();
+}
+});
+
+/* Paginación de pokemon */
+
+const paginacion =() => {
+    let min = document.getElementById("rangoPokemonsMin").value;
+    let max = document.getElementById("rangoPokemonsMax").value;
     
+    if (min > max) {
+        alert('Rango de pokemons incorrecto');
+        return null;
+    }
+
+    const url = `https://pokeapi.co/api/v2/pokemon?limit=${min}&offset=${max}`;
+    fetch(url).then((res) => { // obtiene el resultado
+        if (res.status != "200") {
+            console.log(res);
+            //error al cargar los pokemons
+        }
+        else {
+            return res.json();
+        }
+    }).then((data) => {
+        if (data) {
+            console.log(data);
+        }
+    });
 }
