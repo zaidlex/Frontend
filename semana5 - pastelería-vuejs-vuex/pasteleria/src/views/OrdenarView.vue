a6061977ad429b9624e3e0a5e5f160e30431aac3
Zaid Alejandro <sadx_1997_97728@live.com.mx>
2022-03-27 17:42:28 -0600

    fix: titulo de h2 a h1

---
+++ semana5 - pastelería-vuejs-vuex/pasteleria/src/views/OrdenarView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <ordenar/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import ordenar from '@/components/ordenar.vue'
+
+export default {
+  name: 'PedirPastel',
+  components: {
+    ordenar,
+  }
+}
+</script>
