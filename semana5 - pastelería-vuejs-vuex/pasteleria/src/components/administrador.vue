a5d2e9ace2772ce9d342edd12863c5dadd017960
Zaid Alejandro <sadx_1997_97728@live.com.mx>
2022-03-27 17:48:29 -0600

    add: ruta administrador

---
+++ semana5 - pastelería-vuejs-vuex/pasteleria/src/components/administrador.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h1>Administrador</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "administrador",
+    props: {
+
+    }
+}
+</script>
---
+++ semana5 - pastelería-vuejs-vuex/pasteleria/src/views/AdministradorView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <administrador/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import administrador from '@/components/administrador.vue'
+
+export default {
+  name: 'administrar',
+  components: {
+    administrador,
+  }
+}
+</script>
