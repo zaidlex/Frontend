ce93619067e15d7a03497d4d611f6cb95d256f9e
Zaid Alejandro <sadx_1997_97728@live.com.mx>
2022-03-27 19:34:47 -0600

    add: img de pasteles

--- semana5 - pastelería-vuejs-vuex/pasteleria/src/components/administrador.vue
+++ semana5 - pastelería-vuejs-vuex/pasteleria/src/components/administrador.vue
@@ -1,6 +1,34 @@
 <template>
     <div>
-        <h1>Administrador</h1>
+        <table class="center">
+            <tr>
+                <th>Pedido</th>
+                <th>Sabor de pastel</th>
+                <th>diseño de pastel</th>
+                <th>Nombre</th>
+                <th>Teléfono</th>
+                <th>Correo</th>
+                <th>Precio</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Vainilla</td>
+                <td>monarca</td>
+                <td>Rodrigo Hernández</td>
+                <td>4435673412</td>
+                <td>RodHerr@gmail.com</td>
+                <td>$190.00</td>
+            </tr>
+            <tr>
+                <td>2</td>
+                <td>Vainilla chocolate</td>
+                <td>frutal roseton</td>
+                <td>Daniela Guzmán</td>
+                <td>44456334412</td>
+                <td>daniG@gmail.com</td>
+                <td>$380.00</td>
+            </tr>
+        </table>
     </div>
 </template>
 
--- semana5 - pastelería-vuejs-vuex/pasteleria/src/components/ordenar.vue
+++ semana5 - pastelería-vuejs-vuex/pasteleria/src/components/ordenar.vue
@@ -1,45 +1,50 @@
 <template>
-    <p>Crea tu propio pastel con tus sabores deseados y los diseños que tu escojas.</p>
+    <h2 class="title">Crea tu propio pastel con tus sabores deseados y los diseños que tu escojas.</h2>
     <br>
     <!--Formulario para realizar pedido-->
     <form action="#" method="post">
         <!--checkbox para escoger sabores-->
-        <label for="sabores">Escoge el sabor de tu pastel.</label> <br>
+        <div class="sabores">
+        <label for="saboresInput">Escoge el sabor de tu pastel.</label> <br>
         <input type="checkbox" name="SabVainilla" id="SabVainilla" value="vainilla"> Vainilla
         <input type="checkbox" name="SabChocolate" id="SabChocolate" value="chocolate"> Chocolate
         <input type="checkbox" name="SabFresa" id="SabFresa" value="fresa"> Fresa
         <input type="checkbox" name="SabDurazno" id="SabDurazno" value="durazno"> Durazno
         <input type="checkbox" name="SabLimon" id="SabLimon" value="limon"> Limon
+        </div>
         <br><br>
 
         <!--checkbox para escoger diseños-->
-        <label for="disenos">Escoge el sabor de tu pastel.</label> <br>
-        <input type="checkbox" name="DisMonarca" id="DisMonarca" value="monarca"><img width="100" height="80" src="https://lh6.googleusercontent.com/-S1Qa5nGH62U/VRBCLcrQvDI/AAAAAAABsLA/qzq8cPMQzkw/s800/DSC05808.JPG" alt="La monarca">
-        <br>
-        <input type="checkbox" name="DisRoseton" id="DisRoseton" value="roseton"><img width="100" height="80" src="https://m.media-amazon.com/images/I/712zHoGantL._AC_SX466_.jpg" alt="Rosetón amarillo y rosa">
-        <br>
-        <input type="checkbox" name="DisFrutal" id="DisFrutal" value="frutal"><img width="100" height="80" src="https://image.winudf.com/v2/image1/Y29tLkJpcnRoZGF5Q2FrZS5QYXBhdGhfc2NyZWVuXzRfMTU0NzM1MDA0N18wODg/screen-4.jpg?fakeurl=1&type=.jpg" alt="Frutal">
-        <br>
-        *Nota: al escoger 2 o más diseños, el pastel se dividirá en partes iguales.
-
+        <div class="disenos">
+            <label class="" for="disenosInput">Escoge el sabor de tu pastel.</label> <br>
+            <input type="checkbox" name="DisMonarca" id="DisMonarca" value="monarca"><img width="100" height="80" src="https://lh6.googleusercontent.com/-S1Qa5nGH62U/VRBCLcrQvDI/AAAAAAABsLA/qzq8cPMQzkw/s800/DSC05808.JPG" alt="La monarca">
+            <br>
+            <input type="checkbox" name="DisRoseton" id="DisRoseton" value="roseton"><img width="100" height="80" src="https://m.media-amazon.com/images/I/712zHoGantL._AC_SX466_.jpg" alt="Rosetón amarillo y rosa">
+            <br>
+            <input type="checkbox" name="DisFrutal" id="DisFrutal" value="frutal"><img width="100" height="80" src="https://image.winudf.com/v2/image1/Y29tLkJpcnRoZGF5Q2FrZS5QYXBhdGhfc2NyZWVuXzRfMTU0NzM1MDA0N18wODg/screen-4.jpg?fakeurl=1&type=.jpg" alt="Frutal">
+            <br>
+            <span class="nota">*Nota: al escoger 2 o más diseños, el pastel se dividirá en partes iguales.</span>
+        </div>
         <br><br>
+
         <!--Datos del cliente-->
-        <p>Para poder concluir el pedido, por favor llenar la siguiente información.</p>
-        <label for="Nombre">Nombre:</label>
-        <input type="text" name="Nombre" id="Nombre">
-        <br>
+        <div class="datos">
+            <h2 class="title">Para poder concluir el pedido, por favor llenar la siguiente información.</h2>
+            <label for="Nombre">Nombre:</label>
+            <input type="text" name="Nombre" id="Nombre">
+            <br>
 
-        <label for="Telefono">Teléfono:</label>
-        <input type="tel" name="Telefono" id="Telefono">
-        <br>
+            <label for="Telefono">Teléfono:</label>
+            <input type="tel" name="Telefono" id="Telefono">
+            <br>
 
-        <label for="Correo">Correo electronico:</label>
-        <input type="email" name="Correo" id="Correo">
-        <br>
-
+            <label for="Correo">Correo electronico:</label>
+            <input type="email" name="Correo" id="Correo">
+        </div>
+        
         <input type="reset" value="Limpiar pedido">
         <input type="submit" value="Comprar">
-
+        
     </form>
 </template>
 
@@ -51,3 +56,13 @@ export default {
     }
 }
 </script>
+
+<style scoped>
+.nota {
+    font-size: 12px;
+}
+
+.datos {
+    background-color: rgb(212,172,104,0.6);
+}
+</style>
